c32a6f71aec23276358b1bc2088667ed9e0fc8fe
Stary 2001 <mctinfoilball@gmail.com>
2015-12-20 21:08:57 +0000

    factoid plugin

---
+++ src/factoid/factoid.cpp
@@ -0,0 +1,83 @@
+#include "plugin.h"
+#include "events.h"
+#include "bot.h"
+
+class FactoidPlugin : public Plugin
+{
+public:
+    virtual void init(PluginHost *h);
+    virtual void deinit(PluginHost *h);
+    virtual std::string name();
+
+private:
+    bool factoid(Event *e);
+    Bot *bot;
+};
+
+extern "C" Plugin* plugin_init(PluginHost *h)
+{
+	return new FactoidPlugin();
+}
+
+std::string FactoidPlugin::name()
+{
+	return "factoid";
+}
+
+void FactoidPlugin::init(PluginHost *h)
+{
+	Bot *b = (Bot*)h;
+
+	using namespace std::placeholders;
+	b->add_handler("command/f", "factoid", std::bind(&FactoidPlugin::factoid, this, _1));
+	bot = b;
+}
+
+void FactoidPlugin::deinit(PluginHost *h)
+{
+	bot->remove_handler("command/f", "factoid");
+}
+
+bool FactoidPlugin::factoid(Event *e)
+{
+	IRCCommandEvent *ev = reinterpret_cast<IRCCommandEvent*>(e);
+	std::string usage = "Usage: f [name] [value]";
+	if(ev->params.size() == 0)
+	{
+		bot->conn->send_privmsg(ev->target, usage);
+		return true;
+	}
+
+	if(ev->params.size() == 1)
+	{
+		std::shared_ptr<ConfigNode> v = bot->config->get("factoids." + ev->params[0]);
+		if(v->type() != NodeType::Null)
+		{
+			bot->conn->send_privmsg(ev->target, v->as_map()["value"].string);
+		}
+		else
+		{
+			bot->conn->send_privmsg(ev->target, "Factoid '" + ev->params[0] + "' not found!");
+		}
+	}
+	else
+	{
+		std::string value;
+		std::string n = ev->params[0];
+		ev->params.erase(ev->params.begin());
+
+		for(auto v: ev->params)
+		{
+			value += v + " ";
+		}
+		value = value.substr(0, value.length() - 1);
+		ConfigValue vv;
+		vv.type = NodeType::Map;
+		vv.map["value"] = ConfigValue(value);
+		vv.map["setter"] = ConfigValue(ev->sender->nick);
+
+		bot->config->set("factoids." + n, vv);
+	}
+
+	return true;
+}
